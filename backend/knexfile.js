62e2b9adf8b7fa4ca04b8a6d9ead0eb18346888d
fernandobraga780 <fcfbb1@aluno.ifal.edu.br>
2022-05-03 22:54:03 -0300

    Back-end da aplicação

---
+++ backend/knexfile.js
@@ -0,0 +1,54 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  //AMBIENTE DE DESENVOLVIMENTO
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+  },
+
+  //AMBIENTE DE PRODUÇÃO PARA 
+  //O TIME DE DESENCOLVIMENTO
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  
+  //AMBIENTE DE PRODUÇÃO
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
